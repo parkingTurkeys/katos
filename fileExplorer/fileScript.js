2dfce72b1a5d58260688e54e10ce575b40a10493
parkingTurkeys <roya1060wien@gmail.com>
2024-07-01 06:46:13 +0000

    arcade 1.7.24 8.45
    work

--- fileExplorer/fileScript.js
+++ fileExplorer/fileScript.js
@@ -1,2 +1,12 @@
-
+const fileNames = {
+  "1":"test",
+  "2":"test",
+  "3":"test",
+  "4":"test",
+  "5":"test",
+  "6":"test",
+  "7":"test",
+  "8":"test",
+};
 document.getElementById("speech").innerHTML = "What do you want?";
+
